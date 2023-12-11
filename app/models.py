abf7e69e86c2c1c97c578c4f1e43f35cc720cd4b
P. Prasant Achary <150173869+prasantachary@users.noreply.github.com>
2023-12-12 01:14:46 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Country(models.Model):
+    c_name=models.CharField(max_length=100, primary_key=True)
+class Capital(models.Model):
+    c_name=models.ForeignKey(Country,on_delete=models.CASCADE)
+    capital_name=models.CharField(max_length=100)
+    
